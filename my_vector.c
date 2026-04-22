d1e19f8207c18adb7b9fa83f26d4ff6d6ca7aa4d
shalopai00 <denisukmihas@gmail.com>
2026-04-22 19:38:51 +0300

    first commit

---
+++ my_vector.c
@@ -0,0 +1,46 @@
+#include "my_vector.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+size_t vec_capacity(const Vector* vec) {
+    return vec->capacity;
+}
+size_t vec_size(const Vector* vec) {
+    return vec->size;
+};
+void* vec_data(const Vector* vec) {
+    return vec->data;
+};
+
+Vector* vec_new(size_t element_size) {
+    Vector* vec = (Vector*) malloc(sizeof(Vector));   //Vector* vec = malloc(sizeof(Vector)); ??
+    vec->capacity =0;
+    vec->size = 0;
+    vec->data = NULL;
+    vec->element_size = element_size;
+    return vec;
+};
+
+void vec_free(Vector *vec) {
+    free(vec->data);
+    free(vec);
+};
+
+int vec_push(Vector* vec, const void* element) {
+    if (vec->size >= vec->capacity) {
+        size_t new_capacity = vec->capacity == 0 ? 4 : vec->capacity * 2;
+        void *new_data = realloc(vec->data, new_capacity * vec->element_size);
+        if (new_data == NULL) { return -1;}
+        vec->data = new_data;
+        vec->capacity = new_capacity;
+    }
+    char* dest = (char *) vec->data + vec->size * vec->element_size;
+    memcpy(dest, element, vec->element_size);
+    vec->size++;
+    return 0;
+};
+
+
+
---
+++ my_vector.h
@@ -0,0 +1,23 @@
+#ifndef MY_VECTOR_LIBRARY_H
+#define MY_VECTOR_LIBRARY_H
+#include <stddef.h>
+
+typedef struct{
+    void* data;
+    size_t size;
+    size_t capacity;
+    size_t element_size;
+} Vector;
+
+//геттеры
+size_t vec_capacity(const Vector* vec);
+size_t vec_size(const Vector* vec);
+void* vec_data(const Vector* vec);
+//*
+
+Vector* vec_new(size_t capacity);
+void vec_free(Vector* vec);
+int vec_push(Vector* vec, const void* element);
+void* vec_pop(Vector* vec);
+
+#endif // MY_VECTOR_LIBRARY_H
